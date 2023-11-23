4a20b31918dc68bfe9955c6fad1abbe05cf578e7
realcuesn <129620236+realcuesn@users.noreply.github.com>
2023-11-23 22:12:09 +0800

    feat:firebase vercel push

---
+++ nuxt.config.ts
@@ -0,0 +1,37 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  ssr: true,
+  css: ["~/assets/css/main.css", "~/assets/fonts/stylesheet.css"],
+  build: {
+    transpile: ['gsap']
+  },
+  app: {
+    head: {
+      title: `World's Leading Wet and Dry Auto flush Central Vacuum Cleaner | INVAC - Your Cleaning Partner`,
+      meta: [
+        {
+          name: 'description',
+          content: `Upgrade to a reliable home central vacuum system from INVAC. Discover our selection of affordable vacuum cleaners and experience the convenience of an automatic central vacuum system.`
+        }
+      ]
+    }
+  },
+
+  modules: [
+    "@vueuse/nuxt",
+    "@nuxt/image-edge",
+    "@nuxtjs/robots",
+    'nuxt-simple-sitemap',
+  ],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  site: {
+    url: 'https://www.in-vac.com',
+  },
+
+  devtools: true,
+});
